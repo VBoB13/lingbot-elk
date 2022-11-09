1063e1447dce36931fa8d01f34ad8ca5ffc1f050
Richard Ericson <thedrummer_88@hotmail.com>
2022-11-09 15:50:09 +0800

    Tweak ELASTIC_IP in hope it works now...

--- api/es/__init__.py
+++ api/es/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-ELASTIC_IP = "localhost"  # str(os.environ["ELASTIC_SERVER"])
+ELASTIC_IP = "elasticsearch"  # str(os.environ["ELASTIC_SERVER"])
 
 ELASTIC_HOST = "http://{}:{}".format(
     ELASTIC_IP, os.environ["ELASTIC_PORT"])
