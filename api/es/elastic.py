916792a2a481268e1c4b075d7f06e363cd04a38e
Richard Ericson <thedrummer_88@hotmail.com>
2022-11-11 15:23:56 +0800

    Fixed some search logic.

--- api/es/elastic.py
+++ api/es/elastic.py
@@ -88,7 +88,7 @@ class LingtelliElastic(Elasticsearch):
             self.logger.msg = "Could not search for documents!"
             self.logger.error(str(err))
             raise self.logger from err
-        return dict(resp["hits"]["total"]["hits"])
+        return dict(resp["hits"])
 
     @overload
     def update_index(self, vendors: Vendors):
