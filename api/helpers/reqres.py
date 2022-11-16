e35ba66398a1c058527a917e026cca47a4031530
Richard Ericson <thedrummer_88@hotmail.com>
2022-11-16 10:32:15 +0800

    Fixed small logical error in ElkServiceResponse.

--- api/helpers/reqres.py
+++ api/helpers/reqres.py
@@ -9,8 +9,8 @@ class ElkServiceResponse(JSONResponse):
     def __init__(self, *args, **kwargs):
         content = kwargs.pop('content', None)
         if not content:
-            err = HelperError("MUST provide content for a Response!",
-                              __file__, self.__class__.__name__)
+            err = HelperError(__file__, self.__class__.__name__,
+                              "MUST provide content for a Response!")
             err.error("Key 'content' not found in kwargs.")
             raise err
         super().__init__(content, **kwargs)
