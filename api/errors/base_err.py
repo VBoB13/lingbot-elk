3851edbb68bf7a25c9bdb51cd8631a15f4d55200
Richard Ericson <thedrummer_88@hotmail.com>
2022-11-16 10:23:16 +0800

    Added a func. that checks whether the inquired index exists or not.

--- api/errors/base_err.py
+++ api/errors/base_err.py
@@ -12,12 +12,22 @@ class BaseError(Exception):
         self.file = file.replace(BASE_DIR, "")
         self.cls = cls
         self._msg = msg
+        self._full_msg = None
         self.logger = self._get_logger()
+
+    def __str__(self):
+        return self.msg
 
     def _get_logger(self) -> Logger:
         return Logger(f"{self.file}: {self.cls}")
 
-    def _get_full_msg(self, level: str | int, extra_msg: str = "") -> str:
+    def _get_full_msg(self, level: str | int, extra_msg: str = "") -> None:
+        """
+        Takes the initial parameters of the logger and sets the full_msg attribute
+        to the composed result message.\n
+        `level: str | int[1 | 2 | 3+]` where `1='INFO'`, `2='WARN'` and `3='ERROR'`.\n
+        `extra_msg: str` is a detailed description that appears in the `._full_msg` attribute as `[_msg]... Details: [extra_msg].`
+        """
         color = None
         if level in ('INFO', 1):
             color = Fore.CYAN
@@ -33,8 +43,8 @@ class BaseError(Exception):
             f" | {self.file} | {self.__class__.__name__}" + \
             Fore.RESET + " " + self.msg
         if not extra_msg or not isinstance(extra_msg, str):
-            return full_msg
-        return full_msg + f"\nDetails:\n{extra_msg}"
+            self._full_msg = full_msg
+        self._full_msg = full_msg + f"\nDetails:\n{extra_msg}"
 
     @property
     def msg(self) -> str:
@@ -54,24 +64,24 @@ class BaseError(Exception):
         """
         Log a message (.msg attribute) to console preceded with a |INFO| tag.
         """
-        full_msg = self._get_full_msg('INFO', extra_msg)
-        print(full_msg)
-        self.logger.info(full_msg)
+        self._get_full_msg('INFO', extra_msg)
+        print(self._full_msg)
+        self.logger.info(self._full_msg)
 
     def warn(self, extra_msg: str = "") -> None:
         """
         Log a message (.msg attribute) to console preceded with a |WARN| tag.
         """
-        full_msg = self._get_full_msg('WARN', extra_msg)
-        print(full_msg)
-        self.logger.warn(full_msg)
+        self._get_full_msg('WARN', extra_msg)
+        print(self._full_msg)
+        self.logger.warn(self._full_msg)
 
     def error(self, extra_msg=str(""), orgErr: Exception = None) -> None:
         """
         Log a message (.msg attribute) to console preceded with a |ERROR| tag.
         """
-        full_msg = self._get_full_msg('ERROR', extra_msg)
-        self.logger.error(full_msg)
+        self._get_full_msg('ERROR', extra_msg)
+        self.logger.error(self._full_msg)
         print_tb(self.__traceback__)
         if orgErr:
             print_tb(orgErr.__traceback__)
