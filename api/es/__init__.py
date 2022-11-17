9cbcd51ae9ab735b3c88c719e4165067b4df9431
Richard Ericson <thedrummer_88@hotmail.com>
2022-11-17 14:25:39 +0800

    Added TIIP's official test-index.

--- api/es/__init__.py
+++ api/es/__init__.py
@@ -2,7 +2,9 @@ import os
 
 ELASTIC_IP = os.environ["ELASTIC_SERVER"]
 ELASTIC_PORT = int(os.environ["ELASTIC_PORT"])
+TIIP_INDEX = os.environ["TIIP_INDEX"]
 
 KNOWN_INDEXES = {
-    "tiip-test": {"context": "a"}
+    "tiip-test": {"context": "a"},
+    TIIP_INDEX: {"context": "content"}
 }
