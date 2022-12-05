4e1e57d7986ea0338acdc63be6006b022170f583
Richard Ericson <thedrummer_88@hotmail.com>
2022-12-05 11:26:14 +0800

    Converted simplified chinese to traditional in .dic files for analyzer plugin.

--- elk/create_new_dic.sh
+++ elk/create_new_dic.sh
@@ -1,14 +1,29 @@
-#!/bin/bash
+#!/opt/homebrew/bin/bash
 
 # File meant to create traditional chinese out of simplified chinese
 # *.dic files within the IK analyzer plugin
 
-$FILES = './elasticsearch-analysis-ik-8.3.2/config/*.dic'
+function handle_dics () {
+    # Change directory
+    command cd ./elasticsearch-analysis-ik-8.3.2/config/
+    for f in *.dic
+    do
+        echo "Processing $f"
+        opencc -c s2tw.json -i $f -o /tmp/zhtw-$f
+        cp $f /tmp/zhcn-$f
+        cat /tmp/zhtw-$f /tmp/zhcn-$f | sort | uniq > $f
+    done
+}
 
-for f in $FILES
-do
-    echo "Processing $f"
-    opencc -c s2tw.json -i $f.dic -o /tmp/$f-zhtw.dic
-    cp $f.dic /tmp/$f-zhcn.dic
-    cat /tmp/$f-zhtw.dic /tmp/$f-zhcn.dic | sort | uniq > $f.dic
-done
+DIR=/tmp
+if [ ! -d "$DIR" ];
+then
+    command mkdir "$DIR"
+fi
+
+if [ -d "./elasticsearch-analysis-ik-8.3.2/config" ]; then
+    # If the folder exists, we execute the sctipt
+    handle_dics
+else
+    echo "Cannot process .dic files as folder ${PWD}/elasticsearch-analysis-ik-8.3.2/config doesn't exist!"
+fi
