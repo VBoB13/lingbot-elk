9a7f63a4e6be43b3eb7a668e1f4a9a87b903832e
Richard Ericson <thedrummer_88@hotmail.com>
2022-12-06 17:46:34 +0800

    Changed name on class.

--- api/data/training.py
+++ api/data/training.py
@@ -8,7 +8,11 @@ from data import CLAUDE_TEST_SERVER, OOV_PORT
 from errors.data_err import DataError
 
 
-class Segmenter(object):
+class OOVService(object):
+    """
+    Class meant to handle OOV service tasks.
+    """
+
     def __init__(self, text: str = None):
         self.logger = DataError(__file__, self.__class__.__name__)
         self.server = CLAUDE_TEST_SERVER + ":" + str(OOV_PORT)
@@ -17,9 +21,9 @@ class Segmenter(object):
             self.logger.error(
                 extra_msg="Expected 'str', got '{}'.".format(type(text).__name__))
             raise self.logger
-        self.results = self._segment(text)
+        self.results = self._run(text)
 
-    def _segment(self, text: str = None):
+    def _run(self, text: str = None):
         """
         Method designated to send content to Claude & Rupa's OOV service
         to extract eventual new words, terms and/or phrases and add them into
