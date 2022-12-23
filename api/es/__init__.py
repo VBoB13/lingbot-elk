6a4e3f6855d30a7f82661f90250db458e33596b3
Richard Ericson <thedrummer_88@hotmail.com>
2022-12-23 16:02:18 +0800

    Added new index for new customer.

--- api/es/__init__.py
+++ api/es/__init__.py
@@ -7,7 +7,9 @@ TIIP_INDEX = os.environ["TIIP_INDEX"]
 KNOWN_INDEXES = {
     "tiip-test": {"context": "a"},
     TIIP_INDEX: {"context": "content"},
-    TIIP_INDEX + "-qa": {"context": "a"}
+    TIIP_INDEX + "-qa": {"context": "a"},
+    "7caed8a9-9c02-3b4e-a8eb-94ed959b9b6e": {"context": "content"},
+    "7caed8a9-9c02-3b4e-a8eb-94ed959b9b6e-qa": {"context": "a"}
 }
 
 DEFAULT_ANALYZER = 'ik_max_word'
