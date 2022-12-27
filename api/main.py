7a5a25b2b8e8a496ea06bebdf48217fea2e32571
Richard Ericson <thedrummer_88@hotmail.com>
2022-12-27 14:24:59 +0800

    Changed the responses in /search-gpt to reflect the 'BasicResponse' set as response_model

--- api/main.py
+++ api/main.py
@@ -67,9 +67,9 @@ async def search_doc_gpt(doc: SearchGPT):
         return ElkServiceResponse(content={"msg": "Document(s) found!", "data": result}, status_code=status.HTTP_200_OK)
     except Exception as err:
         if hasattr(err, 'msg') and not es.docs_found:
-            return ElkServiceResponse(content={"error": f"{err.msg}"}, status_code=status.HTTP_204_NO_CONTENT)
+            return ElkServiceResponse(content={"msg": f"{err.msg}", "data": {"error": str(err)}}, status_code=status.HTTP_204_NO_CONTENT)
         es.logger.error(extra_msg=str(err), orgErr=err)
-        return ElkServiceResponse(content={"error": "{}".format(str(err))}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return ElkServiceResponse(content={"msg": "Unexpected ERROR occurred!", "data": {"error": str(err)}}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @app.post("/search/phrase", description=DESCRIPTIONS["/search/phrase"])
