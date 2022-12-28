9d2e90c4b2b168e87a5e7973b129639fa2dc26c9
Richard Ericson <thedrummer_88@hotmail.com>
2022-12-28 14:38:35 +0800

    Initial commit for developing a statistics module for ELK API.

---
+++ api/stats/__init__.py
@@ -0,0 +1,5 @@
+"""
+Module handling all the statistics involved in requests to & from this API project.
+Main goal: Track statistics that are important in terms of monitoring traffic
+            and the way the requests are processed.
+"""
