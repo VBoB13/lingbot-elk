8d98b80b6fbaa95b83ad9f25ed4bb92c9ded6dc5
Richard Ericson <thedrummer_88@hotmail.com>
2023-01-07 11:00:22 +0800

    Made so that the LingtelliElastic.search() method now looks for '-qa' index value and tweaks the 'min_should_match' attribute to maximum tokens from .analyze()

--- api/es/elastic.py
+++ api/es/elastic.py
@@ -290,8 +290,8 @@ class LingtelliElastic(Elasticsearch):
             "text": text,
             "analyzer": analyzer
         }
-        response = requests.post("http://" +
-                                 ELASTIC_IP + ':' + str(ELASTIC_PORT) + '/_analyze', data=data, headers={"Content-Type": "application/json"})
+        response = requests.get("http://" +
+                                ELASTIC_IP + ':' + str(ELASTIC_PORT) + '/_analyze', data=data, headers={"Content-Type": "application/json"})
 
         if response.ok:
             json_resp = response.json()
