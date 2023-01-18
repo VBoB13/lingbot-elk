7e4d74236d0c44a6a966f677153dd1fab71d6339
Richard Ericson <thedrummer_88@hotmail.com>
2023-01-18 17:26:47 +0800

    Changed minimum score for context retrieval.

--- api/es/__init__.py
+++ api/es/__init__.py
@@ -7,7 +7,7 @@ TIIP_INDEX = os.environ["TIIP_INDEX"]
 DEFAULT_ANALYZER = 'icu_analyzer'
 OLD_ANALYZER = 'ik_max_word'
 OLD_SEARCH_ANALYZER = 'ik_smart'
-MIN_DOC_SCORE = 10
+MIN_DOC_SCORE = 8
 MIN_QA_DOC_SCORE = 2
 
 TEXT_FIELD_TYPES = ['text', 'keyword']
