37efead2a98367c0362b6a85319a2de6d138cd9e
Richard Ericson <thedrummer_88@hotmail.com>
2023-02-01 18:16:52 +0800

    Added base file for testing functionalities in GPT-3 project.

--- api/tests/test1.py
+++ api/tests/test1.py
@@ -3,6 +3,27 @@
 
 
 def test_question():
+    # Test a normal question to Chat-GPT
+    return
+
+
+def test_answer():
+    # Test a normal answer from Chat-GPT.
+    return
+
+
+def test_QA():
+    # Test a '-qa' index and check for answers.
+    return
+
+
+def test_delete_index():
+    # Test the 'delete index' function
+    return
+
+
+def test_delete_source():
+    # Test deleting documents from a specific source (file)
     return
 
 
