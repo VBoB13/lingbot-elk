8945209b2d39d0d01adc8916d8c9439f70c6ef56
Richard Ericson <thedrummer_88@hotmail.com>
2023-02-01 15:54:32 +0800

    Added first test file. Now trying to figure out what tests to create.

---
+++ api/tests/test1.py
@@ -0,0 +1,10 @@
+# This module is meant to test so that functionalities within the
+# GPT-3 project and its API is working as intended without crashing.
+
+
+def test_question():
+    return
+
+
+if __name__ == "__main__":
+    print("Starting tests...")
