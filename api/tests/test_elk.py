03914770e9d36930511c2374a3bd1f80199d0211
Richard Ericson <thedrummer_88@hotmail.com>
2023-02-02 11:41:48 +0800

    Using pytest to test the ELK

--- api/tests/test_elk.py
+++ api/tests/test_elk.py
@@ -1,31 +1,23 @@
 # This module is meant to test so that functionalities within the
 # ELK part of the GPT-3 project is working as intended without crashing.
+class TestELK:
 
+    def test_question():
+        # Test a normal question to Chat-GPT
+        return
 
-def test_question():
-    # Test a normal question to Chat-GPT
-    return
+    def test_answer():
+        # Test a normal answer from Chat-GPT.
+        return
 
+    def test_QA():
+        # Test a '-qa' index and check for answers.
+        return
 
-def test_answer():
-    # Test a normal answer from Chat-GPT.
-    return
+    def test_delete_index():
+        # Test the 'delete index' function
+        return
 
-
-def test_QA():
-    # Test a '-qa' index and check for answers.
-    return
-
-
-def test_delete_index():
-    # Test the 'delete index' function
-    return
-
-
-def test_delete_source():
-    # Test deleting documents from a specific source (file)
-    return
-
-
-if __name__ == "__main__":
-    print("Starting tests...")
+    def test_delete_source():
+        # Test deleting documents from a specific source (file)
+        return
