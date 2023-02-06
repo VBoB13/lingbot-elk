1c0ea062f93d42b005efdc1cf46d92f3abac8abd
Richard Ericson <thedrummer_88@hotmail.com>
2023-02-06 10:58:16 +0800

    Added another test for '/search' just to make sure it works.

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -29,7 +29,25 @@ def test_upload_csv():
         assert response.json() == {"msg": "Index deleted.", "data": index}
 
 
-def test_gpt():
+def test_search_gpt():
+    index = "193b3d9c-744c-37d6-bfcb-cc5707cf20d6"
+    data = {
+        "vendor_id": index,
+        "match": {
+            "name": "content",
+            "search_term": "請您直接回覆「ＯＫ！」即可，不用多說什麼。",
+            "operator": "OR",
+            "min_should_match": 1
+        },
+        "strict": False
+    }
+    json_data = json.dumps(data)
+    response = test_client.post("/search-gpt", data=json_data)
+
+    assert response.json()["msg"] == "Document(s) found!"
+
+
+def test_search():
     index = "193b3d9c-744c-37d6-bfcb-cc5707cf20d6"
     data = {
         "vendor_id": index,
@@ -41,7 +59,6 @@ def test_gpt():
         }
     }
     json_data = json.dumps(data)
-
-    response = test_client.post("/search-gpt", data=json_data)
+    response = test_client.post("/search", data=json_data)
 
     assert response.json()["msg"] == "Document(s) found!"
