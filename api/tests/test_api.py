181aa530711e1d74a4f02477fad1179154915bfe
Richard Ericson <thedrummer_88@hotmail.com>
2023-02-07 16:28:36 +0800

    Re-fined testing.

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -10,11 +10,24 @@ from api.main import app
 test_client = TestClient(app)
 
 
+def create_index_with_data(index: str):
+    files = {'file': open(os.path.join(TIIP_CSV_DIR, '通用.csv'), 'rb')}
+    response = test_client.post(
+        '/upload/csv?index=%s' % index, files=files)
+    return response
+
+
+def delete_index(index: str):
+    data = json.dumps({'vendor_id': index})
+    # Delete index if it exists
+    response = test_client.post("/delete", data=data)
+    return response
+
+
 def test_upload_csv():
     index = "test-upload-csv-index"
     # Create file object to throw to endpoint
-    files = {'file': open(os.path.join(TIIP_CSV_DIR, '通用.csv'), 'rb')}
-    response = test_client.post('/upload/csv?index=%s' % index, files=files)
+    response = create_index_with_data(index)
     check_result = {
         "msg": "Documents successfully uploaded & saved into ELK (index: %s)!" % index}
 
@@ -23,9 +36,7 @@ def test_upload_csv():
     # Initiate ELK client
     elk_client = LingtelliElastic()
     if elk_client.index_exists(index):
-        data = json.dumps({'vendor_id': index})
-        # Delete index if it exists
-        response = test_client.post("/delete", data=data)
+        response = delete_index(index)
         assert response.json() == {"msg": "Index deleted.", "data": index}
 
 
@@ -44,9 +55,7 @@ def test_search_gpt():
     }
     # If the index doesn't exist, we create and load it with data.
     if not elk_client.index_exists(index):
-        files = {'file': open(os.path.join(TIIP_CSV_DIR, '通用.csv'), 'rb')}
-        response = test_client.post(
-            '/upload/csv?index=%s' % index, files=files)
+        response = create_index_with_data(index)
         check_result = {
             "msg": "Documents successfully uploaded & saved into ELK (index: %s)!" % index}
 
@@ -58,23 +67,23 @@ def test_search_gpt():
     assert response.json()["msg"] == "Document(s) found!"
 
     if elk_client.index_exists(index):
-        data = json.dumps({'vendor_id': index})
-        # Delete index if it exists
-        response = test_client.post("/delete", data=data)
+        response = delete_index(index)
         assert response.json() == {"msg": "Index deleted.", "data": index}
 
 
 def test_search():
+    elk_client = LingtelliElastic()
     index = "193b3d9c-744c-37d6-bfcb-cc5707cf20d6"
     data = {
         "vendor_id": index,
         "match": {
             "name": "content",
-            "search_term": "請您直接回覆「ＯＫ！」即可，不用多說什麼。",
+            "search_term": "申請補助上限多少？",
             "operator": "OR",
             "min_should_match": 1
         }
     }
+    response = create_index_with_data(index)
     json_data = json.dumps(data)
     response = test_client.post("/search", data=json_data)
 
@@ -87,3 +96,7 @@ def test_search():
     response = test_client.post("/search", data=json_data)
 
     assert response.status_code == 204
+
+    if elk_client.index_exists(index):
+        response = delete_index(index)
+        assert response.json() == {"msg": "Index deleted.", "data": index}
