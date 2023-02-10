755f7a08d13c9a44102aa4c8c839569bff496c58
Richard Ericson <thedrummer_88@hotmail.com>
2023-02-10 16:38:39 +0800

    Lowered MIN_QA_DOC_SCORE to make sure we match all questions when using search_phrase.

--- api/es/__init__.py
+++ api/es/__init__.py
@@ -8,7 +8,7 @@ DEFAULT_ANALYZER = 'icu_analyzer'
 OLD_ANALYZER = 'ik_max_word'
 OLD_SEARCH_ANALYZER = 'ik_smart'
 MIN_DOC_SCORE = 8
-MIN_QA_DOC_SCORE = 2
+MIN_QA_DOC_SCORE = float(1.15)
 
 TEXT_FIELD_TYPES = ['text', 'keyword']
 NUMBER_FIELD_TYPES = ['long', 'integer', 'short', 'byte',
