d6f7aa9ee3a16a0d4c42ca639b8499b074888447
Richard Ericson <thedrummer_88@hotmail.com>
2023-02-18 15:49:16 +0800

    Fixed .save() function where source was incorrectly referred to.

--- api/es/elastic.py
+++ api/es/elastic.py
@@ -455,7 +455,7 @@ class LingtelliElastic(Elasticsearch):
         """
         if isinstance(doc, dict):
             doc = ElasticDoc(
-                vendor_id=doc["vendor_id"], fields=doc["fields"], source=doc["source"])
+                vendor_id=doc["vendor_id"], fields=doc["fields"], source=doc["fields"][-1]["value"])
         try:
             self.doc = self._level_docs(doc)
             resp = self.index(index=doc.vendor_id,
