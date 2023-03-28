7ab8aadb2cca2cbba7c93391cc51af8d3c0f9420
Richard Ericson <thedrummer_88@hotmail.com>
2023-03-28 14:20:51 +0800

    Tweaked create_new_dic.sh script to look for the correct directory.

--- elk/create_new_dic.sh
+++ elk/create_new_dic.sh
@@ -5,7 +5,7 @@
 
 function handle_dics () {
     # Change directory
-    command cd $HOME/elasticsearch-analysis-ik-8.3.3/config/
+    command cd $PWD/elasticsearch-analysis-ik-8.3.3/config/
     for f in *.dic
     do
         echo "Processing $f"
