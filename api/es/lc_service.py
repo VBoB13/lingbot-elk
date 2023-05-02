127fc521b27ead704dfed44455a66d81b9160301
Richard Ericson <thedrummer_88@hotmail.com>
2023-05-02 17:07:11 +0800

    Removed 'return_source_documents' parameter for Tool chains.

--- api/es/lc_service.py
+++ api/es/lc_service.py
@@ -311,10 +311,10 @@ class LingtelliElastic2(Elasticsearch):
                 # Language specific actions
                 if self.language == "EN":
                     chain = ConversationalRetrievalChain.from_llm(
-                        llm=llm, memory=memory, retriever=vectorstore.as_retriever(), max_tokens_limit=3000, return_source_documents=True)
+                        llm=llm, memory=memory, retriever=vectorstore.as_retriever(), max_tokens_limit=3000)
                 else:
                     chain = ConversationalRetrievalChain.from_llm(
-                        llm=llm, memory=memory, retriever=vectorstore.as_retriever(), max_tokens_limit=3000, return_source_documents=True, condense_question_prompt=PromptTemplate.from_template(self.chinese_template)
+                        llm=llm, memory=memory, retriever=vectorstore.as_retriever(), max_tokens_limit=3000, condense_question_prompt=PromptTemplate.from_template(self.chinese_template)
                     )
 
                 tools.append(Tool(
