290cf9bcb0a3398d01814ef0e8336b84af4d4fab
Richard Ericson <thedrummer_88@hotmail.com>
2023-05-16 17:39:03 +0800

    Tweaked the chunk size of uploaded documents splits.

--- api/es/lc_service.py
+++ api/es/lc_service.py
@@ -49,7 +49,7 @@ class FileLoader(object):
         self.csv_content_col = csv_content_col
 
         # Initialize a default splitter for all documents
-        self.splitter = CharacterTextSplitter(chunk_size=250, chunk_overlap=25)
+        self.splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 
         # As filenames in Chinese will disrupt ElasticSearch and the indexing procedure,
         # we make sure that there's NO Chinese within the filename
